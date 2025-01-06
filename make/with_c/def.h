c5595e94a8dad0e1e7ff0a5c531e9b9c4682c9e1
shucai <1374643996@qq.com>
2025-01-06 15:54:06 +0800

    [ADD] change make directory

---
+++ make/with_c/def.h
@@ -0,0 +1,11 @@
+#ifndef __DEF_H_
+#define __DEF_H_
+
+
+typedef struct stu
+{
+				int number;
+				int list;
+};
+
+#endif
---
+++ make/with_c/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "def.h"
+
+int main()
+{
+				stu A = {1,2};
+				printf("%d %d,A.number,A.list);
+				return 0;
+}
